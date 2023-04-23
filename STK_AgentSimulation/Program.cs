b1c388197611fc1f1a1876fa4fc7a133fcbbec2b
Pavol Petrovič <palko.petrovic@gmail.com>
2023-04-23 11:45:59 +0200

    Add project files.

---
+++ STK_AgentSimulation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace STK_AgentSimulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
